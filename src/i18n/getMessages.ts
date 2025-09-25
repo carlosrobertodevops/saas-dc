3a231cc7135eef903754025f7061c2edd061bbc9
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-09-25 17:38:38 -0300

    Ajustes do TW

--- src/i18n/getMessages.ts
+++ src/i18n/getMessages.ts
@@ -3,11 +3,11 @@ import {Locale} from './locales';
 export async function getMessages(locale: Locale) {
   switch (locale) {
     case 'en-us':
-      return (await import('@/messages/en-us.json')).default;
+      return (await import('@/src/messages/en-us.json')).default;
     case 'es-es':
-      return (await import('@/messages/es-es.json')).default;
+      return (await import('@/src/messages/es-es.json')).default;
     case 'pt-br':
     default:
-      return (await import('@/messages/pt-br.json')).default;
+      return (await import('@/src/messages/pt-br.json')).default;
   }
 }
